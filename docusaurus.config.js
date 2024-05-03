2d93268fb9c73d865e9f554a58ce5eff8384656a
whiteflare <skn9x2@gmail.com>
2024-05-03 21:54:18 +0900

    organizationName の指定を追加

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,8 +23,7 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-//  organizationName: 'facebook', // Usually your GitHub org/user name.
-
+  organizationName: 'whiteflare', // Usually your GitHub org/user name.
   projectName: 'vpm-repos', // Usually your repo name.
 
   onBrokenLinks: 'throw',
